63f36b6f44624247018a2735fbff76ff6f7fa0cc
Mattias <mattiaslovkvist@gmail.com>
2016-04-07 10:17:46 +0200

    Added a compile feature and reorganized files

---
+++ compile.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "Clearing old compiled.rc"
+rm compiled.rc
+touch compiled.rc
+echo "Compiling..."
+cat Init.txt >> compiled.rc
+cat HDAMessageColors.txt >> compiled.rc
+cat HDAItemColors.txt >> compiled.rc
+echo '<' >> compiled.rc
+cat SpoilerAlerts.lua >> compiled.rc
+echo '>' >> compiled.rc
+echo '<' >> compiled.rc
+cat HDamage.lua >> compiled.rc
+echo '>' >> compiled.rc
+cat HDAForceMore.txt >> compiled.rc
+cat HDAtravelPre.txt >> compiled.rc
+echo '<' >> compiled.rc
+cat HDAtravel.lua >> compiled.rc
+echo '>' >> compiled.rc
+cat HilariousDeathArtist.txt >> compiled.rc
+echo "Compile finished"
